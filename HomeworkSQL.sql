29b109cf25a9f92cf8596bb49e1d8f77a0ce2f8a
Maounobuki <116195152+Maounobuki@users.noreply.github.com>
2023-07-09 20:27:31 +0300

    5th homework done

--- HomeworkSQL.sql
+++ HomeworkSQL.sql
@@ -379,3 +379,79 @@ SELECT an_name, an_price
 FROM Analysis
 JOIN Orders ON Analysis.an_id = Orders.ord_an
 WHERE ord_datetime BETWEEN '2020-02-05' AND '2020-02-12';
+
+-- Homework 5
+
+USE homework;
+DROP TABLE IF EXISTS cars;
+CREATE TABLE cars
+(
+	id INT NOT NULL PRIMARY KEY,
+ `name` VARCHAR(45),
+    cost INT
+);
+
+INSERT cars
+VALUES
+	(1, "Audi", 52642),
+    (2, "Mercedes", 57127 ),
+    (3, "Skoda", 9000 ),
+    (4, "Volvo", 29000),
+	(5, "Bentley", 350000),
+    (6, "Citroen ", 21000 ), 
+    (7, "Hummer", 41400), 
+    (8, "Volkswagen ", 21600);
+    
+SELECT *
+FROM cars;
+
+-- Создайте представление, в которое попадут автомобили стоимостью до 25 000 долларов
+CREATE OR REPLACE VIEW lowcost_auto
+AS
+SELECT *
+FROM cars
+WHERE cost<25000;
+SELECT * FROM lowcost_auto;
+-- Изменить в существующем представлении порог для стоимости: пусть цена будет до 30 000 долларов (используя оператор ALTER VIEW)
+ALTER VIEW lowcost_auto
+AS
+SELECT *
+FROM cars
+WHERE cost<30000;
+SELECT * FROM lowcost_auto;
+-- Создайте представление, в котором будут только автомобили марки “Шкода” и “Ауди” (аналогично)
+CREATE OR REPLACE VIEW direct_model
+AS
+SELECT *
+FROM cars
+WHERE `name` = "Audi" OR `name` = "Skoda";
+SELECT * FROM direct_model;
+-- Доп задания:
+-- 1* Получить ранжированный список автомобилей по цене в порядке возрастания
+SELECT
+ROW_NUMBER() OVER(ORDER BY cost) `№`,
+cost as `cost`, `name` as `name`
+FROM cars;
+-- 2* Получить топ-3 самых дорогих автомобилей, а также их общую стоимость  
+
+
+SELECT`name`, cost, SUM(cost) OVER()
+FROM cars
+ORDER BY cost DESC
+LIMIT 3;
+
+-- 3* Получить список автомобилей, у которых цена больше предыдущей цены
+SELECT *,                                          
+LAG(name,1) OVER (ORDER BY cost) AS higher_cost    -- По 
+FROM cars;
+
+-- 4* Получить список автомобилей, у которых цена меньше следующей цены
+SELECT name, cost,
+LEAD (name, 1) OVER (ORDER BY cost DESC) AS lower_cost
+FROM cars;
+
+-- 5*Получить список автомобилей, отсортированный по возрастанию цены, и добавить столбец со значением разницы между текущей ценой и ценой следующего автомобиля
+SELECT *,
+cost - LAG(cost)  OVER w AS 'diff' 
+FROM cars
+WINDOW w AS (ORDER BY cost);
